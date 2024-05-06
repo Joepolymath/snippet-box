5c59acf4bce65c9386ae7a224cf5253cd6c5407b
Joshua_Ajagbe <joshua@equally.ai>
2024-05-06 17:17:02 +0100

    query param, method check, http.Error

--- main.go
+++ main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 const PORT = ":4001"
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcom to Snippet Box"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Welcome to Snippet Box"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.Error(w, "Invalid id Query Param", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with an id of %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not Allowed"))
+		// http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed) // or this.
+		return
+	}
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting Server on Port:", PORT)
 	err := http.ListenAndServe(PORT, mux)
